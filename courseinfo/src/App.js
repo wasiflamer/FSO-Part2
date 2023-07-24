3b2a495112ff77919ba3dce288c2a7284587747b
wasiflamer <wasiflamer@gmail.com>
2023-07-24 22:58:18 +0500

    new component added

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,5 +1,18 @@
-const Header = (props) => {
-  return <h1> {props.course.name} </h1>;
+const Course = ({ course }) => {
+  // getting course
+  // now format it below
+
+  console.log(course.name);
+
+  return (
+    <>
+      <Header course_name={course.name} />;
+    </>
+  );
+};
+
+const Header = ({ course_name }) => {
+  return <h1> {course_name} </h1>;
 };
 
 const Part = (props) => {
@@ -44,30 +57,28 @@ const Total = (props) => {
 
 const App = () => {
   const course = {
+    id: 1,
     name: "Half Stack application development",
     parts: [
       {
         name: "Fundamentals of React",
         exercises: 10,
+        id: 1,
       },
       {
         name: "Using props to pass data",
         exercises: 7,
+        id: 2,
       },
       {
         name: "State of a component",
         exercises: 14,
+        id: 3,
       },
     ],
   };
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  );
+  return <Course course={course} />;
 };
 
 export default App;
