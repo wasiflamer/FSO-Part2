c6b3bec2de80aea9280365398e3a86a73c813427
wasiflamer <wasiflamer@gmail.com>
2023-07-27 17:50:43 +0500

    table shangians

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,10 +1,27 @@
-const Course = ({ course }) => {
+const Course = ({ courses }) => {
+  var Column_Heading = ["sd", "rim jon", "kkaf", "awwz"];
+  var data = ["rdf", "kksdff", "dsfwz"];
+  var Column_footer = ["rdsfn", "kdsf", "adf"];
 
   return (
     <>
-      <Header course_name={course.name} />
-      <Content course={course}/>
-      <Total course={course}/>
+      <table>
+        <thead>
+          <tr>
+            <th>{Column_Headin}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{data}</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>{Column_footer}</td>
+          </tr>
+        </tfoot>
+      </table>
     </>
   );
 };
@@ -13,7 +30,7 @@ const Header = ({ course_name }) => {
   return <h1> {course_name} </h1>;
 };
 
-const Part = ({name,exe, id }) => {
+const Part = ({ name, exe, id }) => {
   return (
     <li key={id}>
       {name} {exe}
@@ -21,74 +38,71 @@ const Part = ({name,exe, id }) => {
   );
 };
 
-const Content = ({course}) => {
-
-    return course.parts.map(x => <Part key={x.id} name={x.name} exe={x.exercises}/>);
-
+const Content = ({ course }) => {
+  return course.parts.map((x) => (
+    <Part key={x.id} name={x.name} exe={x.exercises} />
+  ));
 };
 
-const Total = ({course}) => {
-
- let total = course.parts.reduce((sum,x) => {
-    return sum + x.exercises    
+const Total = ({ course }) => {
+  let total = course.parts.reduce((sum, x) => {
+    return sum + x.exercises;
   }, 0);
 
-  return  <h4> Total of { total } Execercises </h4>
-
+  return <h4> Total of {total} Execercises </h4>;
 };
 
 const App = () => {
-  
- const courses = [
+  const courses = [
     {
-      name: 'Half Stack application development',
+      name: "Half Stack application development",
       id: 1,
       parts: [
         {
-          name: 'Fundamentals of React',
+          name: "Fundamentals of React",
           exercises: 10,
-          id: 1
+          id: 1,
         },
         {
-          name: 'Using props to pass data',
+          name: "Using props to pass data",
           exercises: 7,
-          id: 2
+          id: 2,
         },
         {
-          name: 'State of a component',
+          name: "State of a component",
           exercises: 14,
-          id: 3
+          id: 3,
         },
         {
-          name: 'Redux',
+          name: "Redux",
           exercises: 11,
-          id: 4
-        }
-      ]
-    }, 
+          id: 4,
+        },
+      ],
+    },
     {
-      name: 'Node.js',
+      name: "Node.js",
       id: 2,
       parts: [
         {
-          name: 'Routing',
+          name: "Routing",
           exercises: 3,
-          id: 1
+          id: 1,
         },
         {
-          name: 'Middlewares',
+          name: "Middlewares",
           exercises: 7,
-          id: 2
-        }
-      ]
-    }
-  ]
+          id: 2,
+        },
+      ],
+    },
+  ];
 
   return (
-    <div>
-      <Course course={courses}/>
-    </div>
-  )
+    <>
+      <Course courses={courses} />
+    </>
+  );
 };
 
 export default App;
