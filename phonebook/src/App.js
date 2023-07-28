e3872ab08a7e9f51980ee5af189ba9e70fab5da9
wasiflamer <wasiflamer@gmail.com>
2023-07-28 18:56:16 +0500

    fresher code

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,9 +1,26 @@
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
   return (
     <>
-      <h1>phonebook</h1>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+       <div>debug: {newName}</div>
     </>
-  );
+  )
 }
-export default App;
+
+export default App
