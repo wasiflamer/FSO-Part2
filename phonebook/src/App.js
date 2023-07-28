a0d2ede0ae38e7635e067e5f6c81c8f31a0a4619
wasiflamer <wasiflamer@gmail.com>
2023-07-28 21:20:15 +0500

    form logic here and there

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,23 +2,35 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas' },
+    { name: 'Waseem Raza' },
+  ])
   const [newName, setNewName] = useState('')
+
+
+  const handleInputChange = (event) => {
+    setNewName(event.target.value)
+    console.log(newName)
+  };
+
+  const handleInputsubmitted = () => {
+    setPersons([persons.push({name:newName})])
+    
+  };
 
   return (
     <>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleInputsubmitted} method='POST'>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleInputChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-       <div>debug: {newName}</div>
+      {persons.map((x) => <li key={x.name}>{x.name}</li>)}
     </>
   )
 }
