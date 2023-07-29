1c9f38fc984c7e6958520a3d95a8fe427162f22a
wasiflamer <wasiflamer@gmail.com>
2023-07-29 22:06:09 +0500

    implementing search

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,7 +3,10 @@ import { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '03314149234'},
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName,   setNewName]   = useState('')
@@ -68,7 +71,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((x) => <li key={x.name}> {x.name} {x.number}  </li>)}
+      {persons.map((x) => {      
+        return <li key={x.name}> {x.name} {x.number}  </li>
+      })}
     </>
   )
 }
