174ca5e1da6ba7db29416085f0973727cd549671
wasiflamer <wasiflamer@gmail.com>
2023-07-29 21:54:51 +0500

    2.8 done

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-  ])
-  const [newName, setNewName] = useState('')
 
-  const handleInputChange = (event) => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '03314149234'},
+  ])
+
+  const [newName,   setNewName]   = useState('')
+  const [newNumber, setnewNumber] = useState('')
+
+  const handleChangeName = (event) => {
    
     setNewName(event.target.value)
 
@@ -27,27 +30,45 @@ const App = () => {
    
   };
 
-  const handleInputsubmitted = (event) => {
-   
+   const handleChangeNumber = (event) => {
+    setnewNumber(event.target.value)
+  }
+
+  const handleSubmitted = (event) => {
+
     event.preventDefault()
-    setPersons([...persons, { name: newName },])
+
+    if (newName === '' || newNumber === '' ) {
+    return alert('Name or Number cannot be empty ! ')
+    }
+
+    setPersons([...persons, { name: newName, number : newNumber },])
     setNewName('');
-    
+    setnewNumber('');
+
   };
 
+ 
   return (
     <>
       <h2>Phonebook</h2>
-      <form onSubmit={handleInputsubmitted}>
-        <div>
-          name: <input value={newName} onChange={handleInputChange} />
-        </div>
+
+      <form onSubmit={handleSubmitted}>
+         
+          <div>
+            Name <input value={newName} onChange={handleChangeName} />
+          </div>
+
+          <div>
+            Number <input type='number' value={newNumber} onChange={handleChangeNumber} />
+          </div>
+          
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((x) => <li key={x.name}>{x.name}</li>)}
+      {persons.map((x) => <li key={x.name}> {x.name} {x.number}  </li>)}
     </>
   )
 }
